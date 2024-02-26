ac57d265414a61d2ee98e21c2401096e1e1022c4
102sae <102chimee@gmail.com>
2024-02-27 08:18:30 +0900

    add : cell 입력 기능 추가

--- 과제 4. spreadSheet/script.js
+++ 과제 4. spreadSheet/script.js
@@ -116,7 +116,39 @@ function createCellEl(cell) {
     cellEl.classList.add("header");
   }
 
+  cellEl.onclick = () => handleCellClick(cell);
+  cellEl.onchange = (e) => handleOnChange(e.target.value, cell);
+
   return cellEl;
+}
+
+function handleOnChange(data, cell) {
+  cell.data = data;
+}
+
+function handleCellClick(cell) {
+  clearHeaderActiveStates();
+  const columnHeader = spreadsheet[0][cell.column];
+  const rowHeader = spreadsheet[cell.row][0];
+  const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+  const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+  columnHeaderEl.classList.add("active");
+  rowHeaderEl.classList.add("active");
+  // console.log('clicked cell', columnHeaderEl, rowHeaderEl);
+  document.querySelector("#cell-status").innerHTML =
+    cell.columnName + cell.rowName;
+}
+
+function clearHeaderActiveStates() {
+  const headers = document.querySelectorAll(".header");
+
+  headers.forEach((header) => {
+    header.classList.remove("active");
+  });
+}
+
+function getElFromRowCol(row, col) {
+  return document.querySelector("#cell_" + row + col);
 }
 
 function drawSheet() {
