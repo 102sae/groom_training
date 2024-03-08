6f1580fae2b42d2da1342f01b14f75d9000cefa5
102sae <102chimee@gmail.com>
2024-03-08 18:54:55 +0900

    add : row 추가

--- react-netflix/src/App.tsx
+++ react-netflix/src/App.tsx
@@ -1,12 +1,33 @@
 import React from "react";
 import Nav from "./components/Navbar/Nav";
 import Banner from "./components/Banner/Banner";
+import Row from "./components/Row/Row";
+import requests from "./api/requests";
+import "./App.css";
 
 const App: React.FC = () => {
   return (
-    <div>
+    <div className="app">
       <Nav />
       <Banner />
+      <Row
+        title="NETFLIX ORIGINALS"
+        id="NO"
+        fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+      <Row
+        title="Action Movies"
+        id="AM"
+        fetchUrl={requests.fetchActionMovies}
+      />
+      <Row
+        title="Comedy Movies"
+        id="Cm"
+        fetchUrl={requests.fetchComedyMovies}
+      />
     </div>
   );
 };
--- react-netflix/src/components/Row/Row.tsx
+++ react-netflix/src/components/Row/Row.tsx
@@ -1,7 +1,92 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "../../api/axios";
+import "./Row.css";
 
-const Row: React.FC = () => {
-  return <div></div>;
+interface RowProps {
+  isLargeRow?: boolean;
+  title: string;
+  id: string;
+  fetchUrl: string;
+}
+
+interface Movie {
+  id: number;
+  name: string;
+  poster_path: string;
+  backdrop_path: string;
+}
+
+const Row: React.FC<RowProps> = ({
+  isLargeRow = false,
+  title,
+  id,
+  fetchUrl,
+}) => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [movieSelected, setMovieSelected] = useState({});
+
+  useEffect(() => {
+    fetchMovieData();
+  }, []);
+
+  const handleClick = (movie: Movie) => {
+    setModalOpen(true);
+    setMovieSelected(movie);
+  };
+
+  const fetchMovieData = async () => {
+    const request = await axios.get(fetchUrl);
+    console.log("request", request);
+    setMovies(request.data.results);
+  };
+
+  const handleClickLeft = () => {
+    const element = document.getElementById(id);
+    if (element) {
+      // element가 null이 아닐 때만 실행
+      element.scrollLeft -= window.innerWidth - 80;
+    }
+  };
+
+  const handleClickRight = () => {
+    const element = document.getElementById(id);
+    if (element) {
+      // element가 null이 아닐 때만 실행
+      element.scrollLeft += window.innerWidth - 80;
+    }
+  };
+
+  return (
+    <section className="row">
+      <h2>{title}</h2>
+      <div className="slider">
+        <div className="slider__arrow-left">
+          <span className="arrow" onClick={handleClickLeft}>
+            {"<"}
+          </span>
+        </div>
+        <div id={id} className="row__posters">
+          {movies.map((movie) => (
+            <img
+              key={movie.id}
+              className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+              src={`https://image.tmdb.org/t/p/original/${
+                isLargeRow ? movie.poster_path : movie.backdrop_path
+              } `}
+              alt={movie.name}
+              onClick={() => handleClick(movie)}
+            />
+          ))}
+        </div>
+        <div className="slider__arrow-right">
+          <span className="arrow" onClick={handleClickRight}>
+            {">"}
+          </span>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Row;
