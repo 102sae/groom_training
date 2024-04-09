dba1d58c5fd9f408c8b3a0c560cc9da899fb0e27
102sae <102chimee@gmail.com>
2024-04-09 20:01:19 +0900

    add : post 상세 페이지

--- react-atoz/next-js-app/lib/post.ts
+++ react-atoz/next-js-app/lib/post.ts
@@ -2,6 +2,8 @@
 import fs from "fs";
 import path from "path";
 import matter from "gray-matter";
+import { remark } from "remark";
+import remarkHtml from "remark-html";
 
 //post 폴더 경로
 const postsDirectory = path.join(process.cwd(), "posts");
@@ -36,4 +38,35 @@ export function getSortedPostsData() {
       return -1;
     }
   });
+}
+
+export function getAllPostIds() {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  return fileNames.map((fileName) => {
+    return {
+      params: {
+        id: fileName.replace(/\.md$/, ""),
+      },
+    };
+  });
+}
+
+export async function getPostData(id: string) {
+  const fullPath = path.join(postsDirectory, `${id}.md`);
+  const fileContents = fs.readFileSync(fullPath, "utf8");
+
+  const matterResult = matter(fileContents);
+
+  const processdContent = await remark()
+    .use(remarkHtml)
+    .process(matterResult.content);
+
+  const contentHtml = processdContent.toString();
+
+  return {
+    id,
+    contentHtml,
+    ...(matterResult.data as { date: string; title: string }),
+  };
 }
--- react-atoz/next-js-app/pages/posts/[id].tsx
+++ react-atoz/next-js-app/pages/posts/[id].tsx
@@ -1,7 +1,52 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import React from "react";
+import { getAllPostIds, getPostData } from "@/lib/post";
+import Head from "next/head";
+import postStyles from "@/styles/Post.module.css";
+const Post = ({
+  postData,
+}: {
+  postData:
+    | {
+        title: string;
+        date: string;
+        contentHtml: string;
+      }
+    | undefined;
+}) => {
+  if (!postData) {
+    return <div>Loading...</div>; // 데이터가 없을 때 처리
+  }
 
-const Post = () => {
-  return <div></div>;
+  return (
+    <div className={postStyles.container}>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1>{postData.title}</h1>
+        <div>{postData.date}</div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </div>
+  );
 };
 
 export default Post;
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const paths = getAllPostIds();
+  //params = [{id: 'ssg-ssr'}, {id: 'pre-rendering'}]
+  return {
+    paths,
+    fallback: false,
+    //fallback: false 는 paths에 없는 경우 404 페이지를 보여줌
+  };
+};
+
+export const getStaticProps: GetStaticProps = async ({ params = {} }) => {
+  const postData = await getPostData(params?.id as string);
+  return {
+    props: { postData },
+  };
+};
